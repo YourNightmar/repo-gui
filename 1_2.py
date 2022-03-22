b8b7970084c628b8cc0577e4475a3f287a10b0c6
YourNightmar <101049397+YourNightmar@users.noreply.github.com>
2022-03-22 22:36:56 +0300

    Delete 1_2.py

--- 1_2.py
+++
@@ -1,8 +0,0 @@
-#запрос пользователя для ввода числа
-num = int(input('Enter a number: '))
-#расчет в формате чч:мм:сс
-result_1 = num // 3600
-result_2 = (num - (result_1 * 3600)) // 60
-result_3 = (num - (result_1 * 3600) - (result_2 * 60)) % 60
-#выводим результат
-print(f'{result_1}:{result_2}:{result_3}')
